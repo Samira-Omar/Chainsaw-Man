9e2ae542d26b22640276af954b0e3e58c5ab7c39
Samira-Omar <156457740+Samira-Omar@users.noreply.github.com>
2024-12-12 23:53:09 -0600

    Door.cs

--- Scripts/Door.cs
+++ Scripts/Door.cs
@@ -1,77 +1,72 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+// Winta Edited
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Door : MonoBehaviour
+public class DoorInteraction : MonoBehaviour
 {
-    public GameObject handUI;
-    public GameObject UIText;
+    public GameObject interactionPrompt; // UI element to indicate interaction possibility
+    public GameObject warningMessage; // UI element for feedback when the door can't be opened
+    public GameObject playerKey; // Simulates the inventory key
+    public GameObject transitionEffect; // Visual effect for scene transition
+    public string targetScene; // Name of the scene to load
 
-    public GameObject invKey;
-    public GameObject fadeFX;
-
-    public string nextSceneName; // Name of the next scene to load
-
-
-    private bool inReach;
-
+    private bool isPlayerNearby = false; // Tracks whether the player is near the door
 
     void Start()
     {
-        handUI.SetActive(false);
-        UIText.SetActive(false);
-
-        invKey.SetActive(false);
-
-        fadeFX.SetActive(false);
-
-
+        // Initially deactivate all UI elements and effects
+        interactionPrompt.SetActive(false);
+        warningMessage.SetActive(false);
+        playerKey.SetActive(false);
+        transitionEffect.SetActive(false);
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Reach")
+        if (other.CompareTag("Reach"))
         {
-            inReach = true;
-            handUI.SetActive(true);
+            isPlayerNearby = true;
+            interactionPrompt.SetActive(true); // Show interaction prompt when in range
         }
-
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Reach")
+        if (other.CompareTag("Reach"))
         {
-            inReach = false;
-            handUI.SetActive(false);
-            UIText.SetActive(false);
+            isPlayerNearby = false;
+            interactionPrompt.SetActive(false); // Hide interaction prompt when out of range
+            warningMessage.SetActive(false); // Clear warning message
         }
     }
 
     void Update()
     {
-
-
-        if (inReach && Input.GetButtonDown("Interact") && !invKey.activeInHierarchy)
+        // Handle interaction logic
+        if (isPlayerNearby && Input.GetButtonDown("Interact"))
         {
-            handUI.SetActive(true);
-            UIText.SetActive(true);
-        }
-
-        if (inReach && Input.GetButtonDown("Interact") && invKey.activeInHierarchy)
-        {
-            handUI.SetActive(false);
-            UIText.SetActive(false);
-            fadeFX.SetActive(true);
-            StartCoroutine(ending());
+            if (!playerKey.activeInHierarchy)
+            {
+                // If the key is not collected, show a warning
+                warningMessage.SetActive(true);
+            }
+            else
+            {
+                // If the key is present, trigger transition and load the next scene
+                interactionPrompt.SetActive(false);
+                warningMessage.SetActive(false);
+                transitionEffect.SetActive(true);
+                StartCoroutine(LoadTargetScene());
+            }
         }
     }
 
-    IEnumerator ending()
+    IEnumerator LoadTargetScene()
     {
-        yield return new WaitForSeconds(.6f);
-        SceneManager.LoadScene(nextSceneName);
+        // Add a slight delay before transitioning to the next scene
+        yield return new WaitForSeconds(0.6f);
+        SceneManager.LoadScene(targetScene);
     }
-
 }
+
